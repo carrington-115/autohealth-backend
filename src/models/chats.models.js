227f1a4e456e631702980f6c16117cbe5bf4c240
carrington-115 <frumarkcarrington@gmail.com>
2025-11-06 14:10:03 +0530

    updated the modes :

--- src/models/chats.models.js
+++ src/models/chats.models.js
@@ -6,10 +6,6 @@ const chats = new Schema(
       type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
-    message: {
-      type: String,
-      required: true,
-    },
     timestamp: {
       type: Date,
       default: Date.now,
